81f31044bee1a18a9d50dc4ae7b9e97dc561c91e
chencong-plan <1042738887@qq.com>
2017-12-28 21:36:45 +0800

    对insertionSort进行优化

--- InsertionSort/src/cc/ccoder/insertion_sort_advance/Main.java
+++ InsertionSort/src/cc/ccoder/insertion_sort_advance/Main.java
@@ -1,8 +1,22 @@
 package cc.ccoder.insertion_sort_advance;
 
+import java.util.Arrays;
+
 /**
  * @author : ChenCong
  * @date : Created in 17:32 2017/12/28
  */
 public class Main {
+
+    private static final int N = 100000;
+    private static final int RANGE_L = 0;
+    private static final int RANGE_R = 1000000;
+
+    public static void main(String[] args) {
+        // 生成指定的长度的数组  边界为[RANGE_L,RANGE_R]
+        Integer[] arrayInsertion = SortTestHelper.generateRandomArray(N, RANGE_L, RANGE_R);
+        Integer[] arraySelection = Arrays.copyOf(arrayInsertion, arrayInsertion.length);
+        SortTestHelper.testSort(InsertionSort.class, arrayInsertion);
+        SortTestHelper.testSort(SelectionSort.class, arraySelection);
+    }
 }
--- InsertionSort/src/cc/ccoder/insertion_sort_advance/SelectionSort.java
+++ InsertionSort/src/cc/ccoder/insertion_sort_advance/SelectionSort.java
@@ -5,4 +5,33 @@ package cc.ccoder.insertion_sort_advance;
  * @date : Created in 17:32 2017/12/28
  */
 public class SelectionSort {
+
+    private SelectionSort() {
+
+    }
+
+    public static void sort(Comparable[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            int minIndex = i;
+            for (int j = i; j < n; j++) {
+                if (array[j].compareTo(array[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            swap(array, i, minIndex);
+        }
+    }
+
+    private static void swap(Comparable[] array, int i, int minIndex) {
+        Object temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = (Comparable) temp;
+    }
+
+    public static void main(String[] args) {
+        int n = 1000;
+        Integer[] array = SortTestHelper.generateRandomArray(n, 0, 10000);
+        SortTestHelper.testSort(SelectionSort.class, array);
+    }
 }
